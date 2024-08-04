d6d478f72d141ac5418c47120361176d0106b30b
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-08-04 09:34:11 +0100

    criei o componemt table header

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,23 @@
 import { useNavigate } from "react-router-dom"
 import { Button } from "./button"
+import { useState } from "react";
+import { TableHeader } from "./table/table-header";
 
+interface Cursos {
+  id: number;
+  nome: string;
+  categoria: string;
+  professor: string;
+  estado: boolean
+} 
 
 export function CreateCurso(){
-
+   const [cursos, setCursos] = useState<Cursos[]>([
+    {id:1, nome: "react", categoria: "front-end", professor:"Diego Fernandes", estado:false},
+    {id:2, nome: "Java", categoria: "bank-end", professor:"Diego Fernandes", estado:false},
+    {id:3, nome: "Go", categoria: "bank-end", professor:"Diego Fernandes", estado:true},
+    {id:4, nome: "react", categoria: "front-end", professor:"Diego Fernandes", estado:true},
+  ])
   const navigate = useNavigate()
 
   function navigator(){
@@ -17,10 +31,10 @@ export function CreateCurso(){
     <table className="w-[508px] h-52">
             <thead className=" bg-[#7C3AFF]">
                 <tr className=" h-14">
-                    <th className="rounded-tl-lg font-bold px-7 py-4 text-left">Curso</th>
-                    <th className="font-bold px-7 py-4 text-left">Categoria</th>
-                    <th className="font-bold px-7 py-4 text-left">Professor(a)</th>
-                    <th className="rounded-tr-lg font-bold px-7 py-4 text-left">Ativo</th>
+                  <TableHeader rounded="secondary">Curso</TableHeader>
+                  <TableHeader>Categoria</TableHeader>
+                  <TableHeader>Professor(a)</TableHeader>
+                  <TableHeader rounded="terciary">Ativo</TableHeader>
                 </tr>
             </thead>
             <tbody>
@@ -33,7 +47,7 @@ export function CreateCurso(){
                 <tr className=" h-14 border bg-[#212121] border-b-[#292929] border-t-0 border-r-0 border-l-0">
                     <td className="px-7 py-4">Java</td>
                     <td className="px-7 py-4">Back-end</td>
-                    <td className="px-7 py-4">Daniele Leã</td>
+                    <td className="px-7 py-4">Daniele Leão</td>
                     <td className="px-7 py-4">Sim</td>
                 </tr>
                 <tr className=" h-14">
